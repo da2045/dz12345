c87f13d3fb0dfa7fda44f0c4e1ea71643cf6ba7a
da2045 <vitde2009@gmail.com>
2024-08-14 16:10:56 +0300

    Add files via upload

---
+++ dz123.py
@@ -0,0 +1,13 @@
+# dovzuna = int(input("Введіть довжину лінії--. "))
+# symbol = input("Введіть символ для заповнення лінії --> ")
+
+# for _ in range(dovzuna):
+#     print(symbol)
+
+num = int(input('enter your num -->'))
+if num > 0:
+    print("num is positive")
+    if num < 0:
+        print("num is negative")
+        if num == 7:
+            print("Good buy!!!")
