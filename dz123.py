4e0562a04599d19246b77a0076afda1f45d165c7
da2045 <vitde2009@gmail.com>
2024-08-14 16:11:49 +0300

    Update dz123.py

--- dz123.py
+++ dz123.py
@@ -4,10 +4,41 @@
 # for _ in range(dovzuna):
 #     print(symbol)
 
-num = int(input('enter your num -->'))
-if num > 0:
-    print("num is positive")
-    if num < 0:
-        print("num is negative")
-        if num == 7:
-            print("Good buy!!!")
+
+
+
+
+
+# num = int(input('enter your num -->'))
+# if num == 7:
+#     print("Good buy")
+#     if num > 0:
+#         print("num is positive")
+#         if num < 0:
+#             print("num is negative")
+
+
+
+
+
+
+# total_sum = 0
+# max_number = float('-inf')
+# min_number = float('inf')
+
+# number = int(input("Введіть число: "))
+    
+# if number == 7:
+#         print("Good bye!")
+    
+# total_sum += number
+    
+# if number > max_number:
+#         max_number = number
+    
+# if number < min_number:
+#         min_number = number
+
+# print(f"Сума введених чисел: {total_sum}")
+# print(f"Максимальне введене число: {max_number}")
+# print(f"Мінімальне введене число: {min_number}")
